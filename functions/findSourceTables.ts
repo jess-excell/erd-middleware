1377995a84a42bf6af01ae1ed49d3fdf8b5da794
Jess Excell <jessica.excell@hellomass.co>
2026-05-19 16:54:45 +0100

    Add console log for debugging

--- functions/findSourceTables.ts
+++ functions/findSourceTables.ts
@@ -78,6 +78,7 @@ async function findSourceTableData({token, destinationTable, potentialSourceTabl
             ...potentialSource, foundMatchingRecord: foundRecord
         });
     } 
+    console.log("Successfully generated response data.");
     return responseData;
 }
 
